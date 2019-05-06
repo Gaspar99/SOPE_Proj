549bd53cb5dbc5167559fde7231e1f07688200b0
Gaspar99 <pasgas99@hotmail.com>
2019-05-06 17:59:19 +0100

    Started user program

--- Proj2/server.c
+++ Proj2/server.c
@@ -2,31 +2,43 @@
 #include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "constants.h"
+#include "types.h"
+#include "sope.h"
 
-struct account {
-    int balance;
-    char* hashPassword;
-};
-
-struct account bank_accounts[MAX_BANK_ACCOUNTS];
+bank_account_t bank_accounts[MAX_BANK_ACCOUNTS];
+int current_num_accounts = 0;
 
 
-int creat_account(int account_id, int balance, char* password)
+int creat_account(uint32_t account_id, uint32_t balance, char* password)
 {
-    bank_accounts[account_id].balance = balance;
-    if(getHashPassword(password, bank_accounts[account_id].hashPassword)) return 1;
+    bank_accounts[current_num_accounts].account_id = account_id;
+    bank_accounts[current_num_accounts].balance = balance;
+    
+    if (getHashPassword(password, bank_accounts[current_num_accounts].hash)) return 1;
 
+    //Get salt
+
+    current_num_accounts++;
     return 0;
 }
 
 int getHashPassword(char* password, char* hashPassword)
 {
-
+    hashPassword = (char*) malloc(sizeof(char) * HASH_LEN);
 
 
     return 0;
+}
+
+void* process_order(void* arg)
+{
+
+
 }
 
 int main(int argc, char* argv[])
@@ -34,7 +46,7 @@ int main(int argc, char* argv[])
     int nr_bank_offices;
 
     if(argc != 3) {
-        printf("Usage: %s <nr_bank_offices> <password>\n", argv[0]);
+        printf("Usage: %s <nr_bank_offices> <admin_password>\n", argv[0]);
         return 1;
     }
 
@@ -44,13 +56,16 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    creat_account(ADMIN_ACCOUNT_ID, ADMIN_ACCOUNT_BALANCE, argv[2]);
+    if(creat_account(ADMIN_ACCOUNT_ID, ADMIN_ACCOUNT_BALANCE, argv[2])) return 1;
 
-    pthread_t tidf[nr_bank_offices];
+    pthread_t bank_offices_tid[nr_bank_offices];
 
-    //make fifo
+    mkfifo(SERVER_FIFO_PATH, SERVER_FIFO_PERMISSIONS);
 
-    //create threads
+    for(int i = 1; i <= nr_bank_offices; i++) {
+        pthread_create(&bank_offices_tid[i], NULL, process_order, NULL);
+        logBankOfficeOpen(STDOUT_FILENO, i, bank_offices_tid[i]);
+    }
 
 
 
---
+++ Proj2/user.c
@@ -0,0 +1,28 @@
+
+
+#include "constants.h"
+#include "types.h"
+#include "sope.h"
+
+int get_req_header(char* argv[], req_header_t* req_header)
+{
+    req_header->pid = getpid();
+    req_header->account_id = atoi(argv[1]);
+    strcpy(req_header->password, argv[2]);
+    req_header->op_delay_ms = atoi(argv[3]);
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    req_header_t req_header;
+    
+    if(argc != 6)
+    {
+        printf("Usage: %s <account_id> <password> <op_delay> <op_code> <op_args>\n", argv[0]);
+        return 1;
+    }
+
+    get_req_header(argv, &req_header);
+}
